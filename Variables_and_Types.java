ec2cb6268f337be64e1c9bfd247f34ca0006e1fa
killux = Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-07-21 12:30:58 +0100

    Hello, World! Variables and Types Conditionals Arrays

---
+++ Variables_and_Types.java
@@ -0,0 +1,14 @@
+public class Variables_and_Types {
+    public static void main(String[] args) {
+        char h = 'H';
+        short short_number = 3110;
+        String str = " w0r";
+        byte one = 1;
+        float float_number = 2.0f;
+        boolean boll = true;
+
+        String output = "" + h + short_number + str + one + "d" + " " + float_number
+                + " " + boll;
+        System.out.println(output);
+    }
+}
