a4e75fe214f242a097e3adc34219ddbb0f460750
killux = Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-07-21 14:29:13 +0100

    objects

---
+++ Objects.java
@@ -0,0 +1,32 @@
+
+class Point {
+    private double x;
+    private double y;
+    
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public void print() {
+        System.out.println("(" + x + "," + y + ")");
+    }
+
+    void scale(){
+        this.x /= 2;
+        this.y /= 2;
+    }
+    
+    // Your code goes here
+
+}
+
+public class Objects {
+    public static void main(String[] args) {
+        Point p = new Point(32, 32);
+        for (int i = 0; i < 5; i++) {
+            p.scale();
+            p.print();
+        }
+    }
+}
